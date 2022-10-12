dc55f4422211c54b15b56b700fe4276052f8ce4b
PiotrK77 <koterwap@gmail.com>
2022-10-12 13:15:37 +0200

    Task7.py

---
+++ 02-TypesAndVariables/Task10.py
@@ -0,0 +1,4 @@
+int1 = int(input("first number: "))
+int2 = int(input("second number: "))
+
+print(int1 + int2)
---
+++ 02-TypesAndVariables/Task12.py
@@ -0,0 +1,3 @@
+num1 = 5
+numtosecondpower = num1 ** 2
+print(f'The value is {num1}, and {num1} s its second power is {numtosecondpower}')
