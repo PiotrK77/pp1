90919c3531c368040f87c8ff9211b4f7867bc1fb
PiotrK77 <115069636+PiotrK77@users.noreply.github.com>
2022-10-22 15:59:53 +0200

    Delete Task16.py

--- 03-ControlStructures/Task16.py
+++
@@ -1,8 +0,0 @@
-Num1 = int(input("Enter first number: "))
-Num2 = int(input("Enter second number: "))
-
-if Num1 > Num2:
-    print(f"Numbers in ascending order: {Num2}, {Num1}")
-else:
-    print(f"Numbers in ascending order: {Num1}, {Num2}")
-
